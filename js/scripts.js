f3af9a1ed64ec24674f17a6febce4b25f0f46a69
3ssej <3ssejM@gmail.com>
2018-02-28 19:15:57 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var
+  forumButton =		document.getElementById('forum'),
+  voteButton =		document.getElementById('vote'),
+  storeButton =		document.getElementById('store'),
+  BansButton =		document.getElementById('bans');
+
+forumButton.onclick =	function(){location.href='https://discord.gg/y5VzRr8';};
+voteButton.onclick =    function(){location.href='https://mcreal.life/vote';};
+storeButton.onclick =	function(){location.href='https://rlmcstore.craftingstore.net';};
+bansButton.onclick =	function(){location.href='https://mcreal.life/bans';};
